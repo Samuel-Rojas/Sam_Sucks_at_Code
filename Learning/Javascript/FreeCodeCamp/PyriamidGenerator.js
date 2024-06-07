22a6e22d42d6a2ca3cd3ea06606b52e2f042c679
Samuel Rojas <samprogram7@gmail.com>
2024-06-07 13:02:55 -0600

    gradebook programming finished

--- Learning/Javascript/FreeCodeCamp/PyriamidGenerator.js
+++ Learning/Javascript/FreeCodeCamp/PyriamidGenerator.js
@@ -1,48 +1,48 @@
-const character = "!";
-const count = 10;
-const rows = [];
-let inverted = false;
+// const character = "!";
+// const count = 10;
+// const rows = [];
+// let inverted = false;
 
-function padRow(rowNumber, rowCount){
-    return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+// function padRow(rowNumber, rowCount){
+//     return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 
-}
-
-
-for (let i = 1; i <= count; i++) {
-    if(inverted){
-        rows.unshift(padRow(i, count));
-    } else {
-        rows.push(padRow(i, count));
-    }
-}
+// }
 
 
+// for (let i = 1; i <= count; i++) {
+//     if(inverted){
+//         rows.unshift(padRow(i, count));
+//     } else {
+//         rows.push(padRow(i, count));
+//     }
+// }
 
-while(rows.length < count){
+
+
+// while(rows.length < count){
     
-    rows.push(padRow(rows.length + 1, count));
-}
+//     rows.push(padRow(rows.length + 1, count));
+// }
 
 
 
-for (let i = count; i > 0; i--){
-  rows.push(padRow(i, count));
-}
+// for (let i = count; i > 0; i--){
+//   rows.push(padRow(i, count));
+// }
 
 
-const numbers = [1,2,3];
-const shifted = numbers.shift();
-console.log(shifted);
-const unshifted = numbers.shift(5);
-console.log(unshifted);
-console.log(numbers);
+// const numbers = [1,2,3];
+// const shifted = numbers.shift();
+// console.log(shifted);
+// const unshifted = numbers.shift(5);
+// console.log(unshifted);
+// console.log(numbers);
 
 
-let result = " ";
+// let result = " ";
 
-for (const row of rows){
-    result = result + "\n" + row;
-}
+// for (const row of rows){
+//     result = result + "\n" + row;
+// }
 
-console.log(result);
+// console.log(result);
