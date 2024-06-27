0b8394d2aa20a395d8fcf19e52d0a5e0d97ad44b
Samuel Rojas <samprogram7@gmail.com>
2024-06-26 22:22:55 -0600

    added the basic html

---
+++ Projects/BMI_Calc/bmi.js
@@ -0,0 +1,16 @@
+const height = document.getElementById('weight');
+const weight = document.getElementById('height');
+const buttonSubmit = document.getElementById('btn-submit');
+const result = document.getElementById('result');
+
+//Event Listener Goes here 
+buttonSubmit.addEventListener('click', calculateBMI)
+
+//Calcuator function goes here 
+
+const calculateBMI = (weight, height) => {
+    //Inside handles the logic and returns a value to display in the result 
+
+    return bmiValue;
+
+}
