a96f250a261c032ffb3436c848afb4eeb0490c64
Samuel Rojas <samprogram7@gmail.com>
2024-07-01 22:33:00 -0600

    learning addEventListener's

---
+++ Learning/Javascript/Basic_learning/script.js
@@ -0,0 +1,9 @@
+const questionButton = document.getElementById('question');
+const h1Tag = document.getElementById('h1-tag');
+
+questionButton.addEventListener('click', () => {
+    h1Tag.textContent = "hello world";
+});
+
+
+
