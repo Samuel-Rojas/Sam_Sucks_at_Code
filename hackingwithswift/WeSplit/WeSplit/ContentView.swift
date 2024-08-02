08634315e195e6d9f13d96a4bdbb1e6844a9401b
Samuel Rojas <samprogram7@gmail.com>
2024-08-02 04:40:59 -0600

    day 2

--- hackingwithswift/WeSplit/WeSplit/ContentView.swift
+++ hackingwithswift/WeSplit/WeSplit/ContentView.swift
@@ -10,24 +10,44 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var students = ["harry", "jackson", "Charlie"]
-    @State private var student = "harry"
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    let tipPercentages = [ 10, 15, 20, 0]
+    
     
     var body: some View {
-        
         NavigationStack{
             Form{
-                Picker("Select you Student", selection: $student){
-                    ForEach(students, id: \.self){
-                        Text($0)
+                Section{
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                    
+                    Picker("Number of People", selection: $numberOfPeople){
+                        ForEach(2..<100){
+                            Text("\($0) People")
+                        }
                     }
                 }
+                
+                Section ("How much do you want to tip?"){
+                    Picker("Tip Percentage", selection: $tipPercentage){
+                        ForEach(tipPercentages, id: \.self){
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                
+                Section{
+                    Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                }
+                
             }
-            .navigationTitle("Select a Student")
+            .navigationTitle("WeSplit")
             
         }
-       
-        
     }
     
 }
