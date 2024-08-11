830606267bff3a458a1cbc961176cf3c562f4b4d
Samuel Rojas <samprogram7@gmail.com>
2024-08-11 15:49:24 -0600

    finished wordScramble game

---
+++ hackingwithswift/WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Samuel Rojas on 8/11/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
