ad40ff8b33d67374116697054af544f6fd1d5234
Samuel Rojas <samprogram7@gmail.com>
2024-10-09 08:23:17 -0600

    almost finished

--- Swift/oneFocus/Flow.swift
+++ Swift/oneFocus/Flow.swift
@@ -5,140 +5,288 @@
 //  Created by Samuel Rojas on 9/25/24.
 //
 
+
 import SwiftUI
 
 struct Flow: View {
     
-    //25 Minutes in Seconds
     @State private var timeRemaining = 1500
-    
-    //Check whether time is active
+    @State private var selectedTime = 1500 {
+        //Checks each time the timer is active
+        didSet {
+            if !timeIsActive {
+                timeRemaining = selectedTime
+            }
+        }
+    }
     @State private var timeIsActive = false
-    
-    //Use the Timer Class
     @State private var timer: Timer? = nil
-    
-    //Array to store the task
     @State private var tasks = [String]()
-    
-    //The Task in seconds
     @State private var userTask: String = ""
-    
-    //Storing the index of the current index
     @State private var currentEditIndex: Int? = nil
+    @State private var editedTask: String = ""
     
-    //Update the task
-    @State private var editatedTask: String = ""
+    //Options of Times for the user
+    @State private var times = [1500, 1800, 2100, 2700, 3000, 3600, 5400]
     
-    //Array that contains the amount of times
-    @State private var times = [1500, 1800, 2100 , 2700 , 3000, 3600, 5400]
+    //For Animations
+    @State private var isHoveredAddTask = false
+    @State private var isHoveredAddTaskButton = false
+    @State private var isHoveredTimer = false
+    @State private var isHoveredStartBtn = false
+    @State private var isHoveredResetBtn = false
+    
+    
     
     var body: some View {
-        
-        VStack {
-            
-            Text("To-Do")
-                .font(.title2)
-                .padding(.top, 20)
-            
-            Section {
-                TextField("Task for this Flow", text: $userTask)
-                    .frame(width: 200, height: 40)
-                    .cornerRadius(8)
-                    .padding(.bottom, 10)
+        ScrollView{
+            VStack {
                 
-                Button(action: {
-                    withAnimation {
-                        addTask()
-                    }
-                }) {
-                    Text("+")
-                        .font(.system(size: 24, weight: .bold))
-                        .padding()
-                        .background(Color.blue)
-                        .foregroundColor(.white)
-                        .clipShape(Circle())
-                }
+                // Minimalistic To-Do Heading
+                Text("Flow")
+                    .font(.system(size: 20, weight: .semibold, design: .rounded)) // Adjusted font size
+                    .foregroundColor(Color.primary.opacity(0.8))
+                    .padding(.top, 15) // Adjusted padding
+                    .padding(.bottom, 5)
+                    .multilineTextAlignment(.center)
+                    
                 
+                // Task Input Section
+                taskInputSection
+                
+                // Task List Section
+                taskListSection
+                
+                
+                
+                // Timer Section
+                timerSection
             }
             .padding()
-            
-            Section {
-                ForEach(Array(tasks.enumerated()), id: \.element) { index, task in
-                    HStack {
-                        if currentEditIndex == index {
-                            // Task is being edited, show TextField
-                            TextField("Edit Task", text: $editatedTask)
-                                .frame(width: 150, height: 40)
-                                .background(Color.white)
-                                .cornerRadius(8)
-                            
-                            Button(action: {
-                                withAnimation {
-                                    tasks[index] = editatedTask
-                                    currentEditIndex = nil // Exit edit mode
-                                }
-                            }) {
-                                Text("Save")
-                                    .padding(.horizontal, 10)
-                                    .padding(.vertical, 5)
-                                    .background(Color.green)
-                                    .foregroundColor(.white)
-                                    .cornerRadius(5)
-                            }
-                        } else {
-                            // Task is not being edited, show Text view
-                            Text(task)
-                                .frame(maxWidth: .infinity, alignment: .leading)
-                            
-                            Button(action: {
-                                withAnimation {
-                                    currentEditIndex = index
-                                    editatedTask = task // Set the current task to be edited
-                                }
-                            }) {
-                                Text("Edit")
-                                    .padding(.horizontal, 10)
-                                    .padding(.vertical, 5)
-                                    .background(Color.yellow)
-                                    .foregroundColor(.white)
-                                    .cornerRadius(5)
-                            }
-                        }
-                        
-                        // Delete button
-                        Button(action: {
-                            withAnimation {
-                                deleteTask(at: index)
-                            }
-                        }) {
-                            Text("X")
-                                .padding(.horizontal, 10)
-                                .padding(.vertical, 5)
-                                .background(Color.red.opacity(0.6))
-                                .foregroundColor(.white)
-                                .cornerRadius(5)
-                        }
+            .frame(width: 300) // Set a smaller width for the menu bar app
+            .background(Color.white) // Minimal background color
+            .cornerRadius(12) // Rounded corners for the window
+            .shadow(radius: 5) // Optional shadow for better visibility
+        }
+    }
+    
+    // Task Input Section
+    private var taskInputSection: some View {
+        HStack {
+            TextField("Task...", text: $userTask)
+                .padding(8)
+                .background(Color(.white))
+                    .cornerRadius(8)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 8)
+                        .stroke(Color.gray.opacity(0.5), lineWidth: 1)
+                )
+                .frame(height: 30) // Smaller height
+                .padding(.leading, 10)
+                .scaleEffect(isHoveredAddTask ? 1.06 : 1.0)
+                .onHover{ hovered in
+                    withAnimation(.easeInOut){
+                        isHoveredAddTask = hovered
                     }
-                    .padding()
-                    .background(Color.gray.opacity(0.1))
-                    .cornerRadius(10)
-                    .border(Color.gray, width: 0.4)
-                    .transition(.move(edge: .trailing)) // Add transition animation
+                }
+            
+            Button(action: {
+                withAnimation(.spring()) {
+                    addTask()
+                }
+            }) {
+                Image(systemName: "plus")
+                    .font(.system(size: 18)) // Smaller icon
+                    .foregroundColor(.white)
+                    .padding(8)
+                    .background(Color.blue)
+                    .clipShape(Circle())
+            }
+            .buttonStyle(PlainButtonStyle())
+            .padding(.trailing, 10)
+            .scaleEffect(isHoveredAddTaskButton ? 1.2 : 1.0)
+            .onHover{ hovered in
+                withAnimation(.easeInOut){
+                    isHoveredAddTaskButton = hovered
+                }
+            }
+        }
+        
+        .padding(.vertical, 5) // Reduced vertical padding
+    }
+    
+    //Change to Work mode (change background, add work heading, change once start button is hit)
+    
+   
+    
+    // Task List Section
+    private var taskListSection: some View {
+        VStack(spacing: 7) { // Reduced spacing between tasks
+            ForEach(Array(tasks.enumerated()), id: \.element) { index, task in
+                HStack {
+                    if currentEditIndex == index {
+                        taskEditView(index: index)
+                    } else {
+                        taskDisplayView(index: index, task: task)
+                    }
+                }
+                .padding(8) // Reduced padding
+                .background(Color.white)
+                .cornerRadius(8)
+                .transition(.asymmetric(
+                    insertion: .opacity.combined(with: .move(edge: .trailing)),
+                    removal: .opacity.combined(with: .move(edge: .leading))
+                ))
+                .animation(.easeInOut(duration: 1.2), value: tasks)
+            }
+        }
+        .padding(.horizontal, 10)
+    }
+    
+    // Timer Section
+    private var timerSection: some View {
+        VStack(spacing: 15) { // Reduced spacing
+            // Display Time
+            Text(timeString(time: timeIsActive ? timeRemaining : selectedTime))
+                .font(.title) // Adjusted font size
+                .padding()
+                .border(Color.black, width: 3)
+                .background(Color.gray)
+                .cornerRadius(5)
+                .animation(.easeIn, value: timeIsActive)
+                .scaleEffect(isHoveredTimer ? 0.9: 1)
+                .onHover { timerHover in
+                    withAnimation(.easeIn(duration: 0.2)){
+                        isHoveredTimer = timerHover
+                    }
+                }
+            
+            Picker("Select Time", selection: $selectedTime) {
+                ForEach(times, id: \.self) { timeRange in
+                    Text("\(timeRange / 60)")
+                }
+            }
+            .pickerStyle(MenuPickerStyle()) // Minimal Picker Style
+            .frame(width: 100, height: 20)
+            .disabled(timeIsActive)
+            
+            // Timer Controls
+            HStack(spacing: 15) { // Reduced spacing
+                Button(action: {
+                    timeIsActive ? pauseTimer() : startTimer()
+                }) {
+                    Text(timeIsActive ? "Pause" : "Start")
+                        .font(.subheadline) // Smaller font size
+                        .padding(6)
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(8)
+                }
+                .buttonStyle(PlainButtonStyle())
+                .scaleEffect(isHoveredStartBtn ? 1.2 : 1.0)
+                .onHover{ hoverStart in
+                    withAnimation(.easeInOut(duration: 0.6)){
+                        isHoveredStartBtn = hoverStart
+                    }
+                }
+                
+                Button(action: {
+                    resetTimer()
+                }) {
+                    Text("Reset")
+                        .font(.subheadline) // Smaller font size
+                        .foregroundStyle(.white)
+                        .padding(6)
+                        .background(Color.red)
+                        .cornerRadius(8)
+                        .clipped()
+                        
+                        
+                }
+                .buttonStyle(PlainButtonStyle())
+                .scaleEffect(isHoveredResetBtn ? 1.2 : 1.0)
+                .onHover{ hoverReset in
+                    withAnimation(.easeInOut(duration: 0.6)){
+                        isHoveredResetBtn = hoverReset
+                    }
                 }
             }
             
         }
-        .padding()
+        .padding(.vertical,30)
+    
     }
     
-    //Function --> Adds task to the to-do list
+    // Task Display View
+    private func taskDisplayView(index: Int, task: String) -> some View {
+        HStack {
+            Text(task)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.vertical, 6)
+                .padding(.horizontal, 10)
+                .background(Color.white)
+                .cornerRadius(8)
+            
+            // Edit Button
+            Button(action: {
+                withAnimation(.easeInOut(duration: 0.3)) {
+                    currentEditIndex = index
+                    editedTask = task
+                }
+            }) {
+                Image(systemName: "pencil")
+                    .foregroundColor(.yellow)
+                    .font(.system(size: 18)) // Smaller icon size
+            }
+            .padding(.horizontal, 6)
+            
+            // Delete Button
+            Button(action: {
+                withAnimation(.easeOut(duration: 0.2)) {
+                    deleteTask(at: index)
+                }
+            }) {
+                Image(systemName: "trash")
+                    .foregroundColor(.red)
+                    .font(.system(size: 18)) // Smaller icon size
+            }
+            .padding(.horizontal, 5)
+        }
+    }
+    
+    // Task Edit View
+    private func taskEditView(index: Int) -> some View {
+        HStack {
+            TextField("Edit Task", text: $editedTask)
+                .padding(8)
+                .background(Color(.white))
+                .cornerRadius(8)
+                .frame(height: 30) // Smaller height
+            
+            Button(action: {
+                withAnimation(.easeInOut) {
+                    tasks[index] = editedTask
+                    currentEditIndex = nil
+                }
+            }) {
+                Text("Save")
+                    .foregroundColor(.white)
+                    .padding(6)
+                    .background(Color.green)
+                    .cornerRadius(8)
+            }
+            .buttonStyle(PlainButtonStyle())
+        }
+    }
+    
+    // Add Task
     func addTask() {
+        guard !userTask.isEmpty else { return }
         tasks.append(userTask)
         userTask = ""
     }
     
-    //Function --> Deletes task based on the index
+    // Delete Task
     func deleteTask(at index: Int) {
         tasks.remove(at: index)
     }
@@ -150,8 +298,9 @@ struct Flow: View {
     }
     
     func startTimer() {
+        timeRemaining = selectedTime 
         timeIsActive = true
-        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true ) { _ in
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
             if timeRemaining > 0 {
                 timeRemaining -= 1
             } else {
@@ -159,6 +308,7 @@ struct Flow: View {
             }
         }
     }
+
     
     func pauseTimer() {
         timeIsActive = false
@@ -168,7 +318,7 @@ struct Flow: View {
     func resetTimer() {
         timeIsActive = false
         timer?.invalidate()
-        timeRemaining = times[0]
+        timeRemaining = selectedTime
     }
 }
 
